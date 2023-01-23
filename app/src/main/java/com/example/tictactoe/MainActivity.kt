1361aee4a082d5809e8bd2ce6bbed02cbfcff547
C-Annunziato <68083360+C-Annunziato@users.noreply.github.com>
2023-01-22 23:48:19 -0500

    added the board full of squares

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,24 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,44 +30,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TicTacToe(modifier: Modifier = Modifier) {
-    Board()
-//    BoxBoard()
+    Column(modifier = modifier.fillMaxHeight(10f).background(Color.Blue)) {
+        Board()
+        //this is for some controls
+        Column(modifier = Modifier.fillMaxHeight(50f).background(Color.Black)) {}
+    }
 }
 
-//@Composable
-//fun BoxBoard(modifier: Modifier = Modifier) {
-//    Column(
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        modifier = modifier.fillMaxSize()
-//    ) {
-//        Box(
-//            modifier = modifier.border(
-//                4.dp, Color.Black
-//            ).size(60.dp)
-//        )
-//    }
-//}
 
 @Composable
 fun Board(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = modifier
+            .padding(10.dp)
+            .fillMaxSize(),
     ) {
-//        Divider(
-//            modifier = Modifier.width(100.dp),
-//            thickness = 4.dp,
-//            color = Color.Black
-//        )
-//        XX()
-        OO()
+        for (i in 1..3) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp), modifier = Modifier.padding(
+                    10.dp
+                )
+            ) {
+                for (j in 1..3) {
+                    BoxBoard()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun BoxBoard(modifier: Modifier = Modifier) {
+    Column(
+    ) {
+        Box(modifier = modifier
+            .border(4.dp, Color.Black)
+            .size(60.dp)
+            .clickable { })
     }
 }
 
 @Composable
-fun XX (modifier: Modifier = Modifier) {
+fun XX(modifier: Modifier = Modifier) {
     Divider(
         modifier = Modifier
             .width(60.dp)
@@ -91,20 +94,18 @@ fun XX (modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun OO (modifier: Modifier = Modifier) {
-   Canvas(modifier = modifier){
+fun OO(modifier: Modifier = Modifier) {
+    Canvas(modifier = modifier) {
 
-       val canvasWidth = size.width
-       val canvasHeight = size.height
+        val canvasWidth = size.width
+        val canvasHeight = size.height
 
-       drawCircle(radius = 50f, center = Offset(
-           x = canvasWidth / 2,
-           y = canvasHeight / 2
-       ), color = Color.Black,
-       style = Stroke(width = 10f,)
-           )
-
-   }
+        drawCircle(
+            radius = 50f, center = Offset(
+                x = canvasWidth / 2, y = canvasHeight / 2
+            ), color = Color.Black, style = Stroke(width = 10f)
+        )
+    }
 }
 
 @Preview(showBackground = true)
